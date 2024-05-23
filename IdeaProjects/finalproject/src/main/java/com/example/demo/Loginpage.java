7e644871772a7bc4c425395d9db785b9554c9e59
Emad <emad.etehadiup@gmail.com>
2024-05-23 13:04:22 +0430

    add logi for login page

--- IdeaProjects/finalproject/src/main/java/com/example/demo/Loginpage.java
+++ IdeaProjects/finalproject/src/main/java/com/example/demo/Loginpage.java
@@ -1,17 +1,45 @@
 package com.example.demo;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Random;
+import java.util.ResourceBundle;
 import javax.imageio.ImageIO;
 
-public class Loginpage {
+public class Loginpage implements Initializable{
 
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    @FXML private PasswordField Password;
+
+    @FXML private TextField Username , CAPTCHA;
+
+    @FXML private Button forgotpass , signup , submit;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+
+        submit.setOnMouseEntered(e -> submit.setStyle("-fx-background-color: #6e038c; -fx-background-radius: 10;"));
+        submit.setOnMouseExited(e -> submit.setStyle("-fx-background-color:  #7707b8; -fx-background-radius: 10;"));
+
+        signup.setOnMouseEntered(e -> signup.setStyle("-fx-background-color: #025922; -fx-background-radius: 10;"));
+        signup.setOnMouseExited(e -> signup.setStyle("-fx-background-color:  #0c7039; -fx-background-radius: 10;"));
+
+        forgotpass.setOnMouseEntered(e -> forgotpass.setStyle("-fx-background-color:  #474747; -fx-background-radius: 10;"));
+        forgotpass.setOnMouseExited(e -> forgotpass.setStyle("-fx-background-color:   #737a76; -fx-background-radius: 10;"));
+
+    }
     public static void captcha(){
 
         int width = 90;
